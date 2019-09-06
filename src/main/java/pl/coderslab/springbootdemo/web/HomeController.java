a17a241c1c6f3f3b467a6324b7f4b0e41e8cc377
Arkadiusz Migała <arkadiusz.migala@performgroup.com>
2019-09-06 15:27:12 +0200

    Logging sample

--- src/main/java/pl/coderslab/springbootdemo/web/HomeController.java
+++ src/main/java/pl/coderslab/springbootdemo/web/HomeController.java
@@ -1,13 +1,17 @@
 package pl.coderslab.springbootdemo.web;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    private static final Logger log = LogManager.getLogger(HomeController.class);
 
     @GetMapping("/")
     public String hello() {
+        log.debug("Entering controller");
         return "index";
     }
 }
